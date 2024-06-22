1f731141aa82a4a20e592703ab4ca0f9cc90cce4
Gabriel Sandoval <gfsandovalv@gmail.com>
2024-06-22 12:04:24 -0500

    Updating the string summary string so that is implemente by __init__ method of OrgTask

--- org_core.py
+++ org_core.py
@@ -25,7 +25,70 @@ def object_attributes(obj):
     additional_attrs = {attr: getattr(obj, attr) for attr in all_attrs if not callable(getattr(obj, attr)) and not attr.startswith('__')}
     attr_dict.update(additional_attrs)
     return attr_dict
-        
+
+def _date_str(date, keyword=''):
+    date_attrs = date.__dict__
+    summary_lines = ''
+
+    start = date_attrs['_start'] if date_attrs['_start'] else ''
+    end = date_attrs['_end'] if date_attrs['_end'] else ''
+
+    if start and not end:
+        summary_lines += keyword.capitalize() + ' for ' + start.strftime("%B %d, %Y")
+
+    elif end and not start:
+        summary_lines += keyword.capitalize() + ' ending in ' + end.strftime("%B %d, %Y")
+    
+    elif start and end:
+        summary_lines += keyword.capitalize() + ' starting in ' + start.strftime("%B %d, %Y") + ', and is ending in ' + end.strftime("%B %d, %Y")
+
+    return summary_lines    
+
+def markdow_summary(task, string=False):
+    t = task
+    summary_lines = ['# ' + t.heading]
+
+    scheduled = _date_str(task.scheduled, 'scheduled')
+    deadline = _date_str(task.deadline, 'deadline')
+    
+
+    if bool(scheduled):
+        summary_lines.append(scheduled)
+
+    if bool(deadline):
+        summary_lines.append(deadline)
+    
+    if bool(task.clock):
+        clock = _date_str(task.clock[0], 'clock')
+        summary_lines.append(clock)
+
+    summary_lines.append(t.body)
+    
+    # TODO añadir más detalles de las tareas children, ya se tiene el máximo nivel de profundidad, así que se puede iterar fácilmente
+    # children summary, at the momment just headings
+    if t.children:
+        for x in t.children:
+            summary_lines.append('## ' + x.heading)
+    
+    if string:
+        return '\n'.join(summary_lines)
+    else:
+        return summary_lines
+
+def get_max_level(base_node):
+    max_level = 0
+
+    def traverse(node):
+        nonlocal max_level
+        if node.level > max_level:
+            max_level = node.level
+        for child in node.children:
+            traverse(child)
+    
+    traverse(base_node)
+    return max_level
+
+
 class OrgAgenda():
     def __init__(self, file_path) -> None:
         self.root = org_parse.load(file_path)
@@ -55,21 +118,12 @@ class OrgTask(org_parse.node.OrgNode):
         # TODO show children also in markdown format
         self.lists = get_lists(self.body.split('\n')) if self.body else None
         self.max_level = get_max_level(src_obj)
+        self.string_summary = markdow_summary(src_obj, string=True)
+
+
 
     def duplicate_attributes(self, src_obj):
         for key, value in src_obj.__dict__.items():
             setattr(self, key, value)        
 
 
-def get_max_level(base_node):
-    max_level = 0
-
-    def traverse(node):
-        nonlocal max_level
-        if node.level > max_level:
-            max_level = node.level
-        for child in node.children:
-            traverse(child)
-    
-    traverse(base_node)
-    return max_level
